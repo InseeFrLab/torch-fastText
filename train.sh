1c3d85a5d70c7732cb76610fea0d82dd72f02f6a
tomseimandi <tom.seimandi@gmail.com>
2024-01-24 10:19:23 +0000

    Add MLflow run script

---
+++ train.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+export MLFLOW_S3_ENDPOINT_URL="https://minio.lab.sspcloud.fr"
+export MLFLOW_TRACKING_URI="https://user-tseimandi-597803.user.lab.sspcloud.fr"
+export MLFLOW_EXPERIMENT_NAME="torch-fastText"
+
+mlflow run ~/work/torch-fastText/ --env-manager=local \
+    -P remote_server_uri=$MLFLOW_TRACKING_URI \
+    -P experiment_name=$MLFLOW_EXPERIMENT_NAME
