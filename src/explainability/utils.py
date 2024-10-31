a9c1b43c22d91b7249bf1b03afc1f2874565dca1
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2024-10-31 16:47:06 +0000

    modified:   src/experiments.ipynb 	modified:   src/explainability/utils.py

--- src/explainability/utils.py
+++ src/explainability/utils.py
@@ -64,14 +64,14 @@ def map_processed_to_original(processed_words, original_words, n=1, cutoff=0.9):
         cutoff (float): Minimum similarity score for a match.
     
     Returns:
-        Dict[str, Tuple[List[str], List[float]]]: Mapping from original word to tuple of closest processed words and similarity scores.
+        Dict[str, str]: Mapping from original word to the corresponding closest processed word.
     """
 
     # For each word in the original list, find the n closest matching processed words
     word_mapping = {}
 
     for original_word in original_words:
-        original_word_prepro = clean_text_input([original_word])[0]
+        original_word_prepro = clean_text_input([original_word])[0] # Preprocess the original word
 
         if original_word_prepro == '':
             continue
@@ -106,12 +106,28 @@ def test_end_of_word(all_processed_words, word, target_token, next_token):
 
 
 def match_word_to_token_indexes(sentence, tokenized_sentence_tokens):
+
+    """
+    Match words to token indexes in a sentence.
+
+    Args:
+        sentence (str): Preprocessed sentence.
+        tokenized_sentence_tokens (List[str]): List of tokenized sentence tokens.
+
+    Returns:
+        Dict[str, List[int]]: Mapping from word to list of token indexes.
+
+    """
     pointer_token = 0
     res = {}
     processed_words = sentence.split()
+
+    # we know the tokens are in the right order
     for index_word, word in enumerate(sentence.split()):
         res[word] = []
         start = pointer_token
+
+        # while we don't reach the end of the word, get going
         while not test_end_of_word(processed_words, word, tokenized_sentence_tokens[pointer_token],
                                    tokenized_sentence_tokens[pointer_token + 1]):
             pointer_token += 1
@@ -120,6 +136,7 @@ def match_word_to_token_indexes(sentence, tokenized_sentence_tokens):
         end = pointer_token
         res[word] = list(range(start, end))
 
+    # here we arrive at the end of the sentence
     assert tokenized_sentence_tokens[pointer_token] == '</s>'
     end_of_string_position = pointer_token
 
@@ -128,13 +145,14 @@ def match_word_to_token_indexes(sentence, tokenized_sentence_tokens):
     while pointer_token < len(tokenized_sentence_tokens):
         for index_word, word in enumerate(sentence.split()):
             token = tokenized_sentence_tokens[pointer_token]
+            # now, the condition of matching changes: we need to find the word in the token
             if word in token:
                 res[word].append(pointer_token)
         pointer_token += 1
 
     assert pointer_token == len(tokenized_sentence_tokens)
     assert set(sum([v for v in res.values()], [end_of_string_position])) \
-           == set(range(len(tokenized_sentence_tokens)))
+           == set(range(len(tokenized_sentence_tokens))) # verify if all tokens are used
 
     return res
 
@@ -214,9 +232,6 @@ def compute_word_score(word_to_score_dicts, text,  n=5, cutoff=0.75):
 
         mappings.append(mapping)
         for word_to_score in word_to_score_topk: # iteration over top_k (the preds)
-
-            # for i, word in enumerate(original_words):
-            #     original_words[i] = word.replace(',', '')
       
             scores = []
             stopwords_idx = []
@@ -232,8 +247,6 @@ def compute_word_score(word_to_score_dicts, text,  n=5, cutoff=0.75):
             scores = softmax(scores)  # softmax normalization. Length = len(original_words)
             scores[stopwords_idx] = 0
 
-            # scores = scores / np.sum(scores)  # normalization
-
             all_scores_topk.append(scores)  # length top_k
 
         full_all_scores.append(all_scores_topk)  # length = len(text)
@@ -243,6 +256,24 @@ def compute_word_score(word_to_score_dicts, text,  n=5, cutoff=0.75):
 
 def explain_continuous(text, processed_text, tokenized_text_tokens, mappings,
                        word_to_token_idx_dicts, all_attr, top_k):
+
+    """
+    Score explanation at letter level.
+
+    Args:
+        text (List[str]): List of original sentences.
+        processed_text (List[str]): List of preprocessed sentences.
+        tokenized_text_tokens (List[List[str]]): List of tokenized sentences.
+        mappings (List[Dict[str, str]]): List of mappings from original word to preprocessed word.
+        word_to_token_idx_dicts (List[Dict[str, List[int]]]): List of mappings from preprocessed word to token indexes.
+        all_attr (torch.Tensor): Tensor of token scores.
+        top_k (int): Number of top tokens to consider.
+    
+    Returns:
+        np.array: Array of letter scores.
+
+
+    """
 
     for idx, processed_sentence in enumerate(processed_text):
         tokenized_sentence_tokens = tokenized_text_tokens[idx]
@@ -268,21 +299,25 @@ def explain_continuous(text, processed_text, tokenized_text_tokens, mappings,
         all_scores_letter_topk = []
         for k in range(top_k):
             all_scores_letter = []
-            for original_word in original_words:
+            for xxx, original_word in enumerate(original_words):
                 original_word_prepro = clean_text_input([original_word])[0]
                 letters = list(original_word)
                 scores_letter = np.zeros(len(letters))
 
-                if original_word not in original_to_token:
+                if original_word not in original_to_token: # if stopword, 0
                     all_scores_letter.append(scores_letter)
                     continue
+
                 for pos, token in enumerate(original_to_token[original_word]):
                     pos_token = original_to_token_idxs[original_word][pos]
                     tok = preprocess_token(token)[0]
                     score_token = all_attr[idx, k, pos_token].item()
+
+                    # Embed the token at the right indexes of the word
                     sm = SequenceMatcher(None, original_word_prepro, tok)
                     a, _, size = sm.find_longest_match()
                     scores_letter[a:a + size] += score_token
+                    
                 all_scores_letter.append(scores_letter)
             
             all_scores_letter = np.hstack(all_scores_letter)
