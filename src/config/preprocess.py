49f09800bd83df2c5af459bf3d46f2f957614de0
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2024-11-14 16:27:47 +0000

    progress towards dataframe formatting for APE training

--- src/config/preprocess.py
+++ src/config/preprocess.py
@@ -7,6 +7,7 @@ import pandas as pd
 import unidecode
 from nltk.corpus import stopwords as ntlk_stopwords
 from nltk.stem.snowball import SnowballStemmer
+from sklearn.preprocessing import LabelEncoder
 
 
 def clean_text_feature(df: pd.DataFrame, text_feature: str) -> pd.DataFrame:
@@ -95,3 +96,81 @@ def clean_text_input(text: list[str]) -> pd.DataFrame:
 
     # Return clean DataFrame
     return text
+
+def categorize_surface(
+    df: pd.DataFrame, surface_feature_name: int, like_sirene_3: bool = True
+) -> pd.DataFrame:
+    """
+    Categorize the surface of the activity.
+
+    Args:
+        df (pd.DataFrame): DataFrame to categorize.
+        surface_feature_name (str): Name of the surface feature.
+        like_sirene_3 (bool): If True, categorize like Sirene 3.
+
+    Returns:
+        pd.DataFrame: DataFrame with a new column "surf_cat".
+    """
+    df_copy = df.copy()
+    df_copy[surface_feature_name] = df_copy[surface_feature_name].replace("nan", np.nan)
+    df_copy[surface_feature_name] = df_copy[surface_feature_name].astype(float)
+    # Check surface feature exists
+    if surface_feature_name not in df.columns:
+        raise ValueError(f"Surface feature {surface_feature_name} not found in DataFrame.")
+    # Check surface feature is a float variable
+    if not (pd.api.types.is_float_dtype(df_copy[surface_feature_name])):
+        raise ValueError(f"Surface feature {surface_feature_name} must be a float variable.")
+
+    if like_sirene_3:
+        # Categorize the surface
+        df_copy["surf_cat"] = pd.cut(
+            df_copy[surface_feature_name],
+            bins=[0, 120, 400, 2500, np.inf],
+            labels=["1", "2", "3", "4"],
+        ).astype(str)
+    else:
+        # Log transform the surface
+        df_copy["surf_log"] = np.log(df[surface_feature_name])
+
+        # Categorize the surface
+        df_copy["surf_cat"] = pd.cut(
+            df_copy.surf_log,
+            bins=[0, 3, 4, 5, 12],
+            labels=["1", "2", "3", "4"],
+        ).astype(str)
+
+    df_copy[surface_feature_name] = df_copy["surf_cat"].replace("nan", "0")
+    df_copy[surface_feature_name] = df_copy[surface_feature_name].astype(int)
+    df_copy = df_copy.drop(columns=["surf_log", "surf_cat"], errors="ignore")
+    return df_copy
+
+def clean_and_tokenize_df(df):
+    df.fillna("nan", inplace=True)
+    df = df.rename(
+    columns={
+        "evenement_type": "EVT",
+        "cj": "CJ",
+        "activ_nat_et": "NAT",
+        "liasse_type": "TYP",
+        "activ_surf_et": "SRF",
+        "activ_perm_et": "CRT",
+    }
+    )
+
+    y = "apet_finale"
+    text_feature = "libelle"
+    categorical_features = ["EVT", "CJ", "NAT", "TYP", "CRT", "SRF"]
+    cj_mapping = {df["CJ"].unique()[i]: i for i in range(df["CJ"].nunique())}
+    evt_mapping = {df["EVT"].unique()[i]: i for i in range(df["EVT"].nunique())}
+    nat_mapping = {df["NAT"].unique()[i]: i for i in range(df["NAT"].nunique())}
+    typ_mapping = {df["TYP"].unique()[i]: i for i in range(df["TYP"].nunique())}
+    crt_mapping = {df["CRT"].unique()[i]: i for i in range(df["CRT"].nunique())}
+    df["CJ"] = df["CJ"].map(cj_mapping)
+    df["EVT"] = df["EVT"].map(evt_mapping)
+    df["NAT"] = df["NAT"].map(nat_mapping)
+    df["TYP"] = df["TYP"].map(typ_mapping)
+    df["CRT"] = df["CRT"].map(crt_mapping)
+
+    df = categorize_surface(df, "SRF", like_sirene_3=True)
+
+    return df, cj_mapping, evt_mapping, nat_mapping, typ_mapping, crt_mapping
