55575e3a74739f538c91323f710b3af339923a06
Cédric Couralet <cedric.couralet@insee.fr>
2024-11-29 15:00:22 +0100

    reorganize tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,13 +3,16 @@ import unittest
 import pandas as pd 
 import numpy as np
 from sklearn.model_selection import train_test_split
+from pathlib import Path
 
+source_path = Path(__file__).resolve()
+source_dir = source_path.parent
 
 class tftTest(unittest.TestCase):
       
     def __init__(self, methodName = "runTest"):
         super().__init__(methodName)
-        test_data_path = 'test_data/dbpedia_sample_10000.csv'
+        test_data_path = source_dir.as_posix() + '/test_data/dbpedia_sample_1000.csv'
         df_sample = pd.read_csv(test_data_path,header=None)
         self.texts = df_sample[2].tolist()
         labels = df_sample[0].tolist()
@@ -23,7 +26,8 @@ class tftTest(unittest.TestCase):
         max_n=6
         len_word_ngrams=10
         sparse=False 
-        self.torchfasttext = torchFastText.torchFastText(num_buckets=num_buckets,embedding_dim=embedding_dim,num_classes=num_classes,min_count=min_count,min_n=min_n,max_n=max_n,len_word_ngrams=len_word_ngrams,sparse=sparse)
+        self.torchfasttext = torchFastText.torchFastText(num_buckets=num_buckets,embedding_dim=embedding_dim,min_count=min_count,min_n=min_n,max_n=max_n,len_word_ngrams=len_word_ngrams,sparse=sparse)
+
 
 
     def test_init(self):
@@ -35,14 +39,14 @@ class tftTest(unittest.TestCase):
         max_n=6
         len_word_ngrams=10
         sparse=False 
-        self.torchfasttext = torchFastText.torchFastText(num_buckets=num_buckets,embedding_dim=embedding_dim,num_classes=num_classes,min_count=min_count,min_n=min_n,max_n=max_n,len_word_ngrams=len_word_ngrams,sparse=sparse)
+        self.torchfasttext = torchFastText.torchFastText(num_buckets=num_buckets,embedding_dim=embedding_dim,min_count=min_count,min_n=min_n,max_n=max_n,len_word_ngrams=len_word_ngrams,sparse=sparse)
         self.assertTrue(True)
         
     def test_train_no_categorical_variables(self):
         train_texts, val_texts, train_labels, val_labels = train_test_split(
             self.texts, self.labels_num, test_size=0.2, stratify=self.labels_num
         )
-        self.torchfasttext.train(np.asarray(train_texts)[:,None], np.asarray(train_labels)[:,None], np.asarray(val_texts)[:,None],np.asarray(val_labels)[:,None], num_epochs=5, batch_size=64)
+        self.torchfasttext.train(np.asarray(train_texts), np.asarray(train_labels), np.asarray(val_texts),np.asarray(val_labels), num_epochs=5, batch_size=64, lr=0.001, num_workers=4)
         self.assertTrue(True)
         
 if __name__ == '__main__':
