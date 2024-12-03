1bf794d712e43301e4e34bb665d9c09125279b9f
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2024-12-03 17:50:47 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="torchFastText",
-    version="0.1",
+    version="0.0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,  # Ensure additional files like README.md are included
