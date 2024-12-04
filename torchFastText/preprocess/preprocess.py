a0e3586387a7d6f756150fa9ca5aae93f45a70af
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2024-12-04 12:27:15 +0000

    Update preprocess.py to remove unnecessary APE-specific methods and all pandas lagging reference.

--- torchFastText/preprocess/preprocess.py
+++ torchFastText/preprocess/preprocess.py
@@ -6,24 +6,23 @@ import string
 
 import nltk
 import numpy as np
-import pandas as pd
 import unidecode
 from nltk.corpus import stopwords as ntlk_stopwords
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 
 
-def clean_text_feature(text: list[str], remove_stop_words=True) -> pd.DataFrame:
+def clean_text_feature(text: list[str], remove_stop_words=True):
     """
-    Cleans a text feature for pd.DataFrame `df`.
+    Cleans a text feature.
 
     Args:
-        df (pd.DataFrame): DataFrame.
-        text_feature (str): Name of the text feature.
+        text (list[str]): List of text descriptions.
+        remove_stop_words (bool): If True, remove stopwords.
 
     Returns:
-        df (pd.DataFrame): DataFrame.
+        list[str]: List of cleaned text descriptions.
+
     """
     # Define stopwords and stemmer
     nltk.download("stopwords", quiet=True)
@@ -60,85 +59,6 @@ def clean_text_feature(text: list[str], remove_stop_words=True) -> pd.DataFrame:
 
     # Return clean DataFrame
     return text
-
-
-def categorize_surface(
-    df: pd.DataFrame, surface_feature_name: int, like_sirene_3: bool = True
-) -> pd.DataFrame:
-    """
-    Categorize the surface of the activity.
-
-    Args:
-        df (pd.DataFrame): DataFrame to categorize.
-        surface_feature_name (str): Name of the surface feature.
-        like_sirene_3 (bool): If True, categorize like Sirene 3.
-
-    Returns:
-        pd.DataFrame: DataFrame with a new column "surf_cat".
-    """
-    df_copy = df.copy()
-    df_copy[surface_feature_name] = df_copy[surface_feature_name].replace("nan", np.nan)
-    df_copy[surface_feature_name] = df_copy[surface_feature_name].astype(float)
-    # Check surface feature exists
-    if surface_feature_name not in df.columns:
-        raise ValueError(f"Surface feature {surface_feature_name} not found in DataFrame.")
-    # Check surface feature is a float variable
-    if not (pd.api.types.is_float_dtype(df_copy[surface_feature_name])):
-        raise ValueError(f"Surface feature {surface_feature_name} must be a float variable.")
-
-    if like_sirene_3:
-        # Categorize the surface
-        df_copy["surf_cat"] = pd.cut(
-            df_copy[surface_feature_name],
-            bins=[0, 120, 400, 2500, np.inf],
-            labels=["1", "2", "3", "4"],
-        ).astype(str)
-    else:
-        # Log transform the surface
-        df_copy["surf_log"] = np.log(df[surface_feature_name])
-
-        # Categorize the surface
-        df_copy["surf_cat"] = pd.cut(
-            df_copy.surf_log,
-            bins=[0, 3, 4, 5, 12],
-            labels=["1", "2", "3", "4"],
-        ).astype(str)
-
-    df_copy[surface_feature_name] = df_copy["surf_cat"].replace("nan", "0")
-    df_copy[surface_feature_name] = df_copy[surface_feature_name].astype(int)
-    df_copy = df_copy.drop(columns=["surf_log", "surf_cat"], errors="ignore")
-    return df_copy
-
-
-def clean_and_tokenize_df(
-    df,
-    categorical_features=["EVT", "CJ", "NAT", "TYP", "CRT"],
-    text_feature="libelle_processed",
-    label_col="apet_finale",
-):
-    df.fillna("nan", inplace=True)
-
-    df = df.rename(
-        columns={
-            "evenement_type": "EVT",
-            "cj": "CJ",
-            "activ_nat_et": "NAT",
-            "liasse_type": "TYP",
-            "activ_surf_et": "SRF",
-            "activ_perm_et": "CRT",
-        }
-    )
-
-    les = []
-    for col in categorical_features:
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[col])
-        les.append(le)
-
-    df = categorize_surface(df, "SRF", like_sirene_3=True)
-    df = df[[text_feature, "EVT", "CJ", "NAT", "TYP", "SRF", "CRT", label_col]]
-
-    return df, les
 
 
 def stratified_split_rare_labels(X, y, test_size=0.2, min_train_samples=1):
