a6a7680899879be16b1ee6d79a9b2fce132b435f
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2024-12-04 12:30:23 +0000

    Debugged preprocess/__init__.py and test on virtual environment

--- torchFastText/preprocess/__init__.py
+++ torchFastText/preprocess/__init__.py
@@ -2,6 +2,5 @@
 Init script.
 """
 
-from .preprocess import clean_and_tokenize_df as clean_and_tokenize_df
 from .preprocess import clean_text_feature as clean_text_feature
 from .preprocess import stratified_split_rare_labels as stratified_split_rare_labels
